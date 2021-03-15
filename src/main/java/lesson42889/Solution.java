badf32ba08ff60fc98a958626f44bb62dd79a0e7
Gyeong-Han Kim <goslim56@gmail.com>
2021-03-16 00:23:18 +0900

    feat: "실패율" 문제풀이

--- src/main/java/lesson42889/Solution.java
+++ src/main/java/lesson42889/Solution.java
@@ -1,9 +1,59 @@
 package lesson42889;
 
+import java.util.*;
+
 //https://programmers.co.kr/learn/courses/30/lessons/42889
 public class Solution {
+    class FailRateByStage {
+        int stage;
+        double failRate;
+
+        public FailRateByStage(int stage, double failRate) {
+            this.stage = stage;
+            this.failRate = failRate;
+        }
+    }
+
     public int[] solution(int N, int[] stages) {
-        int[] answer = {};
+        int[] answer = new int[N];
+        int[] challenge = new int[N];
+        int[] fail = new int[N];
+        List<FailRateByStage> list = new ArrayList<>();
+        for (int stage :stages) {
+            if (stage > N) {
+                for (int i=0; i<N; i++) {
+                    challenge[i]++;
+                }
+            } else {
+                for (int i=0; i<stage; i++) {
+                    if(i==stage-1) {
+                        fail[i]++;
+                    }
+                    challenge[i]++;
+                }
+            }
+        }
+
+        for (int i=0; i<N; i++) {
+            list.add(new FailRateByStage(i+1,(double) fail[i]/challenge[i]));
+        }
+
+        list.sort((a, b) -> {
+            if (a.failRate < b.failRate)
+                return 1;
+            else if (a.failRate > b.failRate)
+                return -1;
+            else {
+                if (a.stage < b.stage)
+                    return -1;
+                else
+                    return 1;
+            }
+        });
+
+        for (int i = 0; i< N; i++) {
+            answer[i] = list.get(i).stage;
+        }
         return answer;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -9,17 +9,18 @@ public class SolutionTest {
     @Test
     public void 테스트_42889() {
         lesson42889.Solution solution = new lesson42889.Solution();
-//        String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
-//        String answer = "[Prodo님이 들어왔습니다., Ryan님이 들어왔습니다., Prodo님이 나갔습니다., Prodo님이 들어왔습니다.]";
-//        Assertions.assertThat(Arrays.toString(solution.solution(record))).isEqualTo(answer);
+        int N = 5;
+        int[] stage = {2, 1, 2, 6, 2, 4, 3, 3};
+        int[] answer = {3,4,2,1,5};
+        Assertions.assertThat(solution.solution(N,stage)).isEqualTo(answer);
     }
 
     @Test
     public void 테스트_42888() {
         lesson42888.Solution solution = new lesson42888.Solution();
         String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
-        String answer = "[Prodo님이 들어왔습니다., Ryan님이 들어왔습니다., Prodo님이 나갔습니다., Prodo님이 들어왔습니다.]";
-        Assertions.assertThat(Arrays.toString(solution.solution(record))).isEqualTo(answer);
+        String[] answer = {"Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."};
+        Assertions.assertThat(solution.solution(record)).isEqualTo(answer);
     }
 
 }
